773b6fb5bf2dce7b4e6f41709e61040795cc10c4
Arno Heekhout <arno.heekhout@gmail.com>
2025-07-25 23:54:10 +0200

    updated text and gitignore

--- funclip/introduction.py
+++ funclip/introduction.py
@@ -1,11 +1,4 @@
 top_md_1 = ("""
-    <div align="center">
-    <div style="display:flex; gap: 0.25rem;" align="center">
-    FunClip: <a href='https://github.com/alibaba-damo-academy/FunClip'><img src='https://img.shields.io/badge/Github-Code-blue'></a> 
-    🌟Support us: <a href='https://github.com/alibaba-damo-academy/FunClip/stargazers'><img src='https://img.shields.io/github/stars/alibaba-damo-academy/FunClip.svg?style=social'></a>
-    </div>
-    </div>
-    
     Based on Alibaba Tongyi Lab's self-developed and open-sourced [FunASR](https://github.com/alibaba-damo-academy/FunASR) toolkit and Paraformer series models for speech recognition, endpoint detection, punctuation prediction, timestamp prediction, speaker diarization, and hotword customization open-source pipeline
 
     Accurate recognition, freely copy the desired paragraphs, or set speaker identifiers, one-click clipping and subtitle addition
@@ -27,13 +20,6 @@ top_md_3 = ("""Visiting the FunASR project and paper can help you gain in-depth 
     </div>
     """)
 
-top_md_4 = ("""We provide three LLM calling methods in the 'LLM Intelligent Clipping' module,
-            1. Select Alibaba Cloud Bailian platform to call qwen series models via API, which requires you to prepare an API key from the Bailian platform. Please visit [Alibaba Cloud Bailian](https://bailian.console.aliyun.com/#/home);
-            2. Selecting models starting with GPT means calling the official OpenAI API, which requires you to prepare your own secret key and network environment;
-            3. The [gpt4free](https://github.com/xtekky/gpt4free?tab=readme-ov-file) project is also integrated into FunClip, allowing you to call GPT models for free through it;
-            
-            Methods 1 and 2 require entering the corresponding API key in the interface        
-            Method 3 may be very unstable, with long return times or failed result acquisition. You can try multiple times or prepare your own secret key to use methods 1 and 2
-            
+top_md_4 = ("""
             Do not open multiple interfaces on the same port simultaneously, as this will cause file uploads to be very slow or freeze. Simply close other interfaces to resolve this issue
             """)
